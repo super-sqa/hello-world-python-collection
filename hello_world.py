328abb038f430c94ccf605a48aefa043a2c886fa
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2023-05-16 19:40:28 -0700

    just added a new print statement

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,10 @@ print("Hello world from GitHub")
 print("I am a basic script leaving in github that just prints gibrish")
 
 print("I just added a new line in the file")
+
+
+import os
+
+current_build = os.environ.get('BUILD_NUMBER', "Unable to find the build number variable")
+
+print(f"The build number is: {current_build}")
